c758e9adf8a22b6876b54df5f68b31f49b439281
vkill <vkill.net@gmail.com>
2020-10-01 15:06:08 +0800

    Update AsyncTransportConfiguration

--- src/configuration.rs
+++ src/configuration.rs
@@ -7,10 +7,10 @@ pub struct AsyncTransportConfiguration<H>
 where
     H: ResponseHandler,
 {
-    buf_size: Option<usize>,
-    max_buf_size: Option<usize>,
-    read_timeout: Option<Duration>,
-    max_parse_response_bytes_count: Option<u8>,
+    buf_size: usize,
+    max_buf_size: usize,
+    read_timeout: Duration,
+    max_parse_response_bytes_count: u8,
     pub(crate) response_handler: H,
 }
 
@@ -20,44 +20,48 @@ where
 {
     pub fn new(response_handler: H) -> Self {
         Self {
-            buf_size: None,
-            max_buf_size: None,
-            read_timeout: None,
-            max_parse_response_bytes_count: None,
+            buf_size: 1024,
+            max_buf_size: 1024 * 4,
+            read_timeout: Duration::from_secs(5),
+            max_parse_response_bytes_count: 3,
             response_handler,
         }
     }
 
     pub fn set_buf_size(&mut self, size: usize) {
-        self.buf_size = Some(size);
+        debug_assert!(size <= self.max_buf_size);
+        self.buf_size = size;
     }
 
     pub fn get_buf_size(&self) -> usize {
-        self.buf_size.unwrap_or(1024)
+        self.buf_size
     }
 
     pub fn set_max_buf_size(&mut self, size: usize) {
-        self.max_buf_size = Some(size);
+        debug_assert!(size >= self.buf_size);
+        self.max_buf_size = size;
     }
 
     pub fn get_max_buf_size(&self) -> usize {
-        self.max_buf_size.unwrap_or(1024 * 16)
+        self.max_buf_size
     }
 
     pub fn set_read_timeout(&mut self, timeout_ms: u32) {
-        self.read_timeout = Some(Duration::from_millis(timeout_ms as u64));
+        debug_assert!(timeout_ms > 0);
+        self.read_timeout = Duration::from_millis(timeout_ms as u64);
     }
 
     pub fn get_read_timeout(&self) -> Duration {
-        self.read_timeout.unwrap_or(Duration::from_secs(5))
+        self.read_timeout
     }
 
     pub fn set_max_parse_response_bytes_count(&mut self, size: u8) {
-        self.max_parse_response_bytes_count = Some(size);
+        debug_assert!(size > 0);
+        self.max_parse_response_bytes_count = size;
     }
 
     pub fn get_max_parse_response_bytes_count(&self) -> u8 {
-        self.max_parse_response_bytes_count.unwrap_or(3)
+        self.max_parse_response_bytes_count
     }
 }
 
@@ -76,12 +80,22 @@ mod tests {
     use std::io;
 
     #[test]
-    fn with_default() -> io::Result<()> {
-        let c: DefaultAsyncTransportConfiguration = Default::default();
+    fn get_and_set() -> io::Result<()> {
+        let mut c: DefaultAsyncTransportConfiguration = Default::default();
 
         assert_eq!(c.get_buf_size(), 1024);
+        assert_eq!(c.get_max_buf_size(), 1024 * 4);
         assert_eq!(c.get_read_timeout(), Duration::from_secs(5));
         assert_eq!(c.get_max_parse_response_bytes_count(), 3);
+
+        c.set_buf_size(1024 * 2);
+        assert_eq!(c.get_buf_size(), 1024 * 2);
+        c.set_max_buf_size(1024 * 3);
+        assert_eq!(c.get_max_buf_size(), 1024 * 3);
+        c.set_read_timeout(3000);
+        assert_eq!(c.get_read_timeout(), Duration::from_secs(3));
+        c.set_max_parse_response_bytes_count(2);
+        assert_eq!(c.get_max_parse_response_bytes_count(), 2);
 
         Ok(())
     }

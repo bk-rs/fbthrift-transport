ca6ee237df73f6c6df79a6acf52acaa52b4927a9
vkill <vkill.net@gmail.com>
2021-01-12 11:55:32 +0800

    Support tokio 1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,40 @@
-#[cfg(all(feature = "futures_io", not(feature = "tokio02_io")))]
+#[cfg(all(
+    feature = "futures_io",
+    not(feature = "tokio02_io"),
+    not(feature = "tokio_io")
+))]
 pub mod transport;
-#[cfg(all(feature = "futures_io", not(feature = "tokio02_io")))]
+#[cfg(all(
+    feature = "futures_io",
+    not(feature = "tokio02_io"),
+    not(feature = "tokio_io")
+))]
 pub use transport::AsyncTransport;
 
-#[cfg(all(not(feature = "futures_io"), feature = "tokio02_io"))]
+#[cfg(all(
+    not(feature = "futures_io"),
+    feature = "tokio02_io",
+    not(feature = "tokio_io")
+))]
 pub mod transport;
-#[cfg(all(not(feature = "futures_io"), feature = "tokio02_io"))]
+#[cfg(all(
+    not(feature = "futures_io"),
+    feature = "tokio02_io",
+    not(feature = "tokio_io")
+))]
+pub use transport::AsyncTransport;
+
+#[cfg(all(
+    not(feature = "futures_io"),
+    not(feature = "tokio02_io"),
+    feature = "tokio_io"
+))]
+pub mod transport;
+#[cfg(all(
+    not(feature = "futures_io"),
+    not(feature = "tokio02_io"),
+    feature = "tokio_io"
+))]
 pub use transport::AsyncTransport;
 
 pub mod configuration;
